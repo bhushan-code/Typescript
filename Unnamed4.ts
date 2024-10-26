287a7b0a9eee4f462a6388c73045f0dfa74286ce
bhushan-code <nagpureb7@gmail.com>
2024-10-26 17:26:41 +0530

    Lambda Statement anonymous function

--- Unnamed4.ts
+++ Unnamed4.ts
@@ -1,11 +1,15 @@
 
 //Lambda Statement
 
-var TEMP = (No1 : number , No2 : number) => No1 + No2
-
+var TEMP = (No1 : number , No2 : number) =>
+{
+    console.log("Inside the Addition Lambda Statement")
+    var Result : number = 0
+    Result = No1 + No2
+    return Result
+}
 
 var Ans : number = 0
-
-Ans = TEMP(11,10)
+Ans = TEMP(10,11)
 
 console.log("Addition is : "+Ans)
