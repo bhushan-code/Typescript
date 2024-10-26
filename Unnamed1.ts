39ca93c5d3feb0cd5ce27c9bbcde7a1b046dc1e5
bhushan-code <nagpureb7@gmail.com>
2024-10-26 17:23:23 +0530

    anonymous function

---
+++ Unnamed1.ts
@@ -0,0 +1,11 @@
+
+function Addition(No1 : number , No2 : number) : number
+{
+    return No1 + No2
+}
+
+var Ans : number
+
+Ans = Addition(11,10)
+
+console.log("Addition is "+Ans)
---
+++ Unnamed4.ts
@@ -0,0 +1,11 @@
+
+//Lambda Statement
+
+var TEMP = (No1 : number , No2 : number) => No1 + No2
+
+
+var Ans : number = 0
+
+Ans = TEMP(11,10)
+
+console.log("Addition is : "+Ans)
